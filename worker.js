1583036fd70f3edf9acc4372be170e112ce79cba
AleksMG <5978497@gmail.com>
2025-05-16 19:53:14 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,137 +1,124 @@
-class K4Worker {
-    constructor() {
-        this.terminate = false;
-        this.alphabet = 'KRYPTOSABCDEFGHIJLMNQUVWXZ';
-        this.knownText = '';
-        this.keyGenerator = null;
-        this.expectedFreq = {
-            K: 12.5, R: 10.2, Y: 8.1, P: 7.9, 
-            T: 7.5, O: 6.8, S: 6.5, A: 5.9, B: 5.3
-        };
+const KRYPTOS_ALPHABET = 'KRYPTOSABCDEFGHIJLMNQUVWXZ';
+const ENGLISH_FREQ = {
+    E: 12.70, T: 9.10, A: 8.20, O: 7.50, I: 6.90,
+    N: 6.70, S: 6.30, H: 6.10, R: 6.00, D: 4.30,
+    L: 3.50, C: 2.80, U: 2.80, M: 2.40, W: 2.40,
+    F: 2.20, G: 2.00, Y: 2.00, P: 1.90, B: 1.50,
+    V: 1.00, K: 0.80, J: 0.20, X: 0.20, Q: 0.10,
+    Z: 0.10
+};
+const KNOWN_PATTERNS = ['BERLIN', 'CLOCK', 'NORTHEAST'];
 
-        self.onmessage = (e) => this.handleMessage(e.data);
+class K4Cracker {
+    constructor(config) {
+        this.ciphertext = config.ciphertext;
+        this.alphabet = config.alphabet;
+        this.keyLength = config.keyLength;
+        this.workerId = config.workerId;
+        this.keysGenerated = 0;
+        this.running = true;
     }
 
-    handleMessage(msg) {
-        switch (msg.type) {
-            case 'START':
-                this.terminate = false;
-                this.alphabet = msg.config.alphabet;
-                this.knownText = msg.config.knownText;
-                this.startAttack(msg.config);
-                break;
-                
-            case 'STOP':
-                this.terminate = true;
-                break;
-        }
-    }
-
-    async startAttack(config) {
-        const startTime = Date.now();
-        let keysProcessed = 0;
-        
-        this.keyGenerator = this.generateKeys(config.keyLength);
-        
-        while (!this.terminate && (Date.now() - startTime) < config.timeout) {
-            const { value: key, done } = this.keyGenerator.next();
-            if (done) break;
-
-            const result = this.processKey(key, config.ciphertext);
-            keysProcessed++;
+    start() {
+        try {
+            const keyGenerator = this.generateKeys();
             
-            if (result.score > 50) {
-                self.postMessage({
-                    type: 'RESULT',
-                    data: result
-                });
-            }
+            for (const key of keyGenerator) {
+                if (!this.running) break;
+                
+                this.keysGenerated++;
+                const decrypted = this.vigenereDecrypt(key);
+                const score = this.calculateScore(decrypted);
+                
+                if (score > 85) {
+                    self.postMessage({
+                        type: 'RESULT',
+                        key,
+                        text: decrypted,
+                        score,
+                        entropy: this.calculateEntropy(decrypted)
+                    });
+                }
 
-            if (keysProcessed % 100 === 0) {
-                self.postMessage({
-                    type: 'PROGRESS',
-                    data: { keysProcessed }
-                });
-                keysProcessed = 0;
+                if (this.keysGenerated % 100 === 0) {
+                    self.postMessage({
+                        type: 'PROGRESS',
+                        keysProcessed: this.keysGenerated
+                    });
+                }
             }
+        } catch (error) {
+            self.postMessage({
+                type: 'ERROR',
+                message: `Worker ${this.workerId} crashed: ${error.message}`
+            });
         }
     }
 
-    *generateKeys(keyLength) {
-        const baseChars = this.alphabet.split('');
+    *generateKeys() {
+        const chars = this.alphabet.split('');
+        const key = new Array(this.keyLength).fill(chars[0]);
         
-        function* generate(index, current) {
-            if (index === keyLength) {
-                yield current.join('');
-                return;
+        while (this.running) {
+            yield key.join('');
+            
+            let i = this.keyLength - 1;
+            while (i >= 0) {
+                const currentIndex = chars.indexOf(key[i]);
+                if (currentIndex < chars.length - 1) {
+                    key[i] = chars[currentIndex + 1];
+                    break;
+                } else {
+                    key[i] = chars[0];
+                    i--;
+                }
             }
-            for (const char of baseChars) {
-                current[index] = char;
-                yield* generate(index + 1, current);
-            }
+            if (i < 0) break;
         }
-
-        yield* generate(0, new Array(keyLength));
     }
 
-    processKey(key, ciphertext) {
-        const decrypted = this.vigenereDecrypt(ciphertext, key);
-        return {
-            key,
-            text: decrypted,
-            score: this.calculateScore(decrypted)
-        };
-    }
-
-    vigenereDecrypt(text, key) {
-        return Array.from(text, (c, i) => {
-            const textIndex = this.alphabet.indexOf(c);
-            const keyIndex = this.alphabet.indexOf(key[i % key.length]);
-            if (textIndex === -1 || keyIndex === -1) return c;
+    vigenereDecrypt(key) {
+        return Array.from(this.ciphertext, (char, index) => {
+            const textIndex = this.alphabet.indexOf(char);
+            const keyIndex = this.alphabet.indexOf(key[index % key.length]);
             return this.alphabet[(textIndex - keyIndex + 26) % 26];
         }).join('');
     }
 
     calculateScore(text) {
         let score = 0;
-        const textLength = text.length;
         
-        // Frequency analysis
-        const freqMap = {};
-        for (const c of text) {
-            freqMap[c] = (freqMap[c] || 0) + 1;
-        }
+        // Частотный анализ
+        score += [...text].reduce((sum, char) => 
+            sum + (ENGLISH_FREQ[char] || 0), 0);
         
-        // Frequency match
-        for (const [char, expected] of Object.entries(this.expectedFreq)) {
-            const actual = ((freqMap[char] || 0) / textLength) * 100;
-            score += Math.max(0, 100 - Math.abs(actual - expected));
-        }
+        // Известные паттерны
+        score += KNOWN_PATTERNS.reduce((sum, pattern) => 
+            text.includes(pattern) ? sum + 150 : sum, 0);
+        
+        // Энтропия
+        score -= this.calculateEntropy(text) * 10;
+        
+        return score;
+    }
 
-        // Known text match
-        if (this.knownText && text.includes(this.knownText)) {
-            score += 200;
-        }
-
-        // Kryptos patterns
-        const patterns = [/BERLIN/, /CLOCK/, /NORTHEAST/];
-        patterns.forEach(pattern => {
-            if (pattern.test(text)) {
-                score += 150;
-            }
-        });
-
-        // Penalty for rare patterns
-        const rarePatterns = [/Q{2,}/, /Z{2,}/, /X{2,}/];
-        rarePatterns.forEach(pattern => {
-            if (pattern.test(text)) {
-                score -= 100;
-            }
-        });
-
-        return Math.round(score);
+    calculateEntropy(text) {
+        const freq = {};
+        const len = text.length;
+        [...text].forEach(c => freq[c] = (freq[c] || 0) + 1);
+        return -Object.values(freq).reduce((sum, count) => {
+            const p = count / len;
+            return sum + p * Math.log2(p);
+        }, 0);
     }
 }
 
-// Worker initialization
-new K4Worker();
+self.onmessage = function(e) {
+    if (e.data.type === 'INIT') {
+        const cracker = new K4Cracker(e.data);
+        cracker.start();
+    }
+    if (e.data.type === 'TERMINATE') {
+        cracker.running = false;
+    }
+};
