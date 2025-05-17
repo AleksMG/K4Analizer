397577475e770e02fe6b2b3605e0aba440630da4
AleksMG <5978497@gmail.com>
2025-05-17 18:20:35 +0300

    OSNOVA worker.js

--- worker.js
+++ worker.js
@@ -7,16 +7,24 @@ const ENGLISH_FREQ = {
     'Z': 0.074
 };
 
-// Hyper-optimized precomputed data
-const FREQ_VECTOR = new Float32Array(Object.values(ENGLISH_FREQ));
-const PATTERNS = new Map([
-    [3, ['THE', 'AND', 'YOU', 'BUT', 'HIS', 'HER', 'WAS', 'FOR', 'NOT']],
-    [4, ['THAT', 'WITH', 'HAVE', 'THIS', 'WILL', 'YOUR', 'FROM', 'THEY']],
-    [5, ['WHICH', 'THERE', 'THEIR', 'ABOUT', 'WOULD', 'COULD', 'SHOULD']],
-    [6, ['BECAUSE', 'PEOPLE', 'NUMBER', 'SYSTEM', 'SECRET', 'KRYPTOS']]
-]);
+// Предварительные вычисления для оптимизации
+const ENGLISH_FREQ_ARRAY = new Float32Array(Object.values(ENGLISH_FREQ));
+const COMMON_REGEX = [
+    'THE', 'AND', 'THAT', 'HAVE', 'FOR', 'NOT', 'WITH', 'YOU', 'THIS', 'BUT',
+    'HIS', 'FROM', 'THEY', 'WILL', 'WOULD', 'THERE', 'THEIR', 'WHAT', 'ABOUT',
+    'WHICH', 'WHEN', 'YOUR', 'WERE', 'BERLIN', 'CLOCK', 'EAST', 'NORTH', 'WEST',
+    'SOUTH', 'NORTHEAST', 'NORTHWEST', 'SOUTHEAST', 'SOUTHWEST', 'SECRET', 'CODE',
+    'MESSAGE', 'KRYPTOS', 'CIA', 'AGENT', 'COMPASS', 'LIGHT', 'LATITUDE',
+    'LONGITUDE', 'COORDINATE', 'SHADOW', 'WALL', 'UNDERGROUND'
+].map(p => new RegExp(p, 'g'));
 
-class K4TurboWorker {
+// Предрасчитанная таблица модуля 26
+const MOD26 = new Int8Array(512);
+for (let i = 0; i < MOD26.length; i++) {
+    MOD26[i] = (i % 26 + 26) % 26;
+}
+
+class K4Worker {
     constructor() {
         this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         this.charMap = new Uint8Array(256);
@@ -28,8 +36,9 @@ class K4TurboWorker {
         this.totalWorkers = 1;
         this.keysTested = 0;
         this.startTime = 0;
+        this.lastReportTime = 0;
         
-        // Precompute character map
+        // Инициализация charMap (оптимизировано)
         const A = 'A'.charCodeAt(0);
         for (let i = 0; i < 26; i++) {
             this.charMap[A + i] = i;
@@ -42,12 +51,16 @@ class K4TurboWorker {
         switch (msg.type) {
             case 'init':
                 Object.assign(this, msg);
+                this.keysTested = 0;
                 break;
+                
             case 'start':
                 this.running = true;
                 this.startTime = performance.now();
+                this.lastReportTime = this.startTime;
                 this.bruteForce();
                 break;
+                
             case 'stop':
                 this.running = false;
                 break;
@@ -55,138 +68,140 @@ class K4TurboWorker {
     }
 
     bruteForce() {
+        const totalKeys = 26 ** this.keyLength;
+        const keysPerWorker = Math.ceil(totalKeys / this.totalWorkers);
+        const startKey = this.workerId * keysPerWorker;
+        const endKey = Math.min(startKey + keysPerWorker, totalKeys);
+        const cipherLen = this.ciphertext.length;
         const cipherCodes = this.precomputeCipher();
-        const cipherLen = cipherCodes.length;
         const keyBuffer = new Uint8Array(this.keyLength);
-        const plainBuffer = new Uint8Array(cipherLen);
-        const keySpace = 26 ** this.keyLength;
-        const chunkSize = this.calculateChunkSize();
-
+        const plainCodes = new Uint8Array(cipherLen);
+        const checkKnown = this.knownPlaintext.length > 0;
+        const knownPattern = checkKnown ? new RegExp(this.knownPlaintext) : null;
+        
         let bestScore = 0;
-        let baseKey = this.workerId;
+        let bestKey = '';
+        const CHUNK_SIZE = 100000;
+        let currentKey = startKey;
 
-        while (this.running && baseKey < keySpace) {
-            this.processChunk(baseKey, Math.min(baseKey + chunkSize, keySpace), 
-                cipherCodes, keyBuffer, plainBuffer);
-            baseKey += chunkSize * this.totalWorkers;
-        }
-
-        self.postMessage({ type: 'complete' });
-    }
-
-    processChunk(start, end, cipherCodes, keyBuffer, plainBuffer) {
-        const keyCodes = new Uint8Array(this.keyLength);
-        const kpCheck = this.knownPlaintext ? 
-            new Uint8Array(this.knownPlaintext.split('').map(c => c.charCodeAt(0))) : null;
-
-        for (let keyNum = start; keyNum < end && this.running; keyNum++) {
-            this.generateKey(keyNum, keyBuffer);
-            this.decrypt(cipherCodes, keyBuffer, plainBuffer);
+        while (this.running && currentKey < endKey) {
+            const chunkEnd = Math.min(currentKey + CHUNK_SIZE, endKey);
             
-            const score = this.quantumScore(plainBuffer, kpCheck);
-            this.keysTested++;
-
-            if (score > bestScore) {
-                bestScore = score;
-                const key = String.fromCharCode(...keyBuffer.map(c => c + 65));
-                const plaintext = String.fromCharCode(...plainBuffer);
-                self.postMessage({ type: 'result', key, plaintext, score });
+            for (let keyNum = currentKey; keyNum < chunkEnd; keyNum++) {
+                const key = this.generateKeyOptimized(keyNum, keyBuffer);
+                this.decryptOptimized(cipherCodes, key, plainCodes);
+                
+                const plaintext = String.fromCharCode(...plainCodes);
+                const score = this.scoreOptimized(plaintext, checkKnown, knownPattern);
+                
+                this.keysTested++;
+                
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestKey = key;
+                    self.postMessage({
+                        type: 'result',
+                        key,
+                        plaintext,
+                        score
+                    });
+                }
             }
-
-            if (performance.now() - this.startTime > 1000) {
-                this.reportProgress();
-                this.startTime = performance.now();
-            }
+            
+            this.reportProgressIfNeeded();
+            currentKey = chunkEnd;
+        }
+        
+        if (this.running) {
+            self.postMessage({ type: 'complete' });
         }
     }
 
     precomputeCipher() {
-        const codes = new Uint8Array(this.ciphertext.length);
+        const cipherCodes = new Uint8Array(this.ciphertext.length);
         const A = 'A'.charCodeAt(0);
-        for (let i = 0; i < codes.length; i++) {
-            codes[i] = this.ciphertext.charCodeAt(i) - A;
+        for (let i = 0; i < this.ciphertext.length; i++) {
+            cipherCodes[i] = this.ciphertext.charCodeAt(i) - A;
         }
-        return codes;
+        return cipherCodes;
     }
 
-    generateKey(num, buffer) {
-        let n = num;
+    generateKeyOptimized(num, buffer) {
+        let remaining = num;
         for (let i = this.keyLength - 1; i >= 0; i--) {
-            buffer[i] = n % 26;
-            n = Math.floor(n / 26);
+            buffer[i] = remaining % 26;
+            remaining = Math.floor(remaining / 26);
         }
+        return String.fromCharCode(...buffer.map(c => c + 65));
     }
 
-    decrypt(cipher, key, output) {
-        const keyLen = key.length;
-        for (let i = 0; i < cipher.length; i++) {
-            output[i] = (cipher[i] - key[i % keyLen] + 26) % 26 + 65;
+    decryptOptimized(cipherCodes, key, plainCodes) {
+        const keyCodes = new Uint8Array(key.length);
+        const A = 'A'.charCodeAt(0);
+        for (let i = 0; i < key.length; i++) {
+            keyCodes[i] = key.charCodeAt(i) - A;
         }
-    }
-
-    quantumScore(buffer, kpCheck) {
-        let score = 0;
-        const len = buffer.length;
-        const freq = new Uint16Array(26);
         
-        // 1. Known plaintext check
-        if (kpCheck) {
-            let found = false;
-            outer: for (let i = 0; i <= len - kpCheck.length; i++) {
-                for (let j = 0; j < kpCheck.length; j++) {
-                    if (buffer[i + j] !== kpCheck[j]) continue outer;
-                }
-                found = true;
-                break;
-            }
-            if (!found) return 0;
-            score += 1000 * kpCheck.length;
+        const keyLen = key.length;
+        for (let i = 0; i < cipherCodes.length; i++) {
+            plainCodes[i] = MOD26[cipherCodes[i] - keyCodes[i % keyLen] + 26];
         }
+        
+        for (let i = 0; i < plainCodes.length; i++) {
+            plainCodes[i] += 65;
+        }
+    }
 
-        // 2. Frequency analysis
-        let total = 0;
+    scoreOptimized(text, checkKnown, knownPattern) {
+        if (checkKnown && !knownPattern.test(text)) return 0;
+        
+        let score = 0;
+        const freq = new Uint16Array(26);
+        let totalLetters = 0;
+        const len = text.length;
+        
+        // Частотный анализ
         for (let i = 0; i < len; i++) {
-            const c = buffer[i] - 65;
-            if (c < 0 || c > 25) continue;
-            freq[c]++;
-            total++;
+            const code = text.charCodeAt(i) - 65;
+            if (code < 0 || code > 25) continue;
+            freq[code]++;
+            totalLetters++;
         }
-
-        if (total > 0) {
-            const multiplier = 100 / total;
+        
+        if (totalLetters > 0) {
+            const multiplier = 100 / totalLetters;
             for (let i = 0; i < 26; i++) {
-                score += 100 - Math.abs(FREQ_VECTOR[i] - freq[i] * multiplier);
+                score += 100 - Math.abs(ENGLISH_FREQ_ARRAY[i] - freq[i] * multiplier);
             }
         }
-
-        // 3. Pattern matching
-        const str = String.fromCharCode(...buffer);
-        for (const [len, patterns] of PATTERNS) {
-            for (const p of patterns) {
-                let pos = -1;
-                while ((pos = str.indexOf(p, pos + 1)) !== -1) {
-                    score += len * 50;
-                }
-            }
+        
+        // Проверка паттернов
+        for (const re of COMMON_REGEX) {
+            re.lastIndex = 0;
+            let count = 0;
+            while (re.test(text)) count++;
+            score += count * re.source.length * 25;
         }
-
-        return Math.round(score);
+        
+        if (checkKnown) {
+            score += 1000 * this.knownPlaintext.length;
+        }
+        
+        return score | 0;
     }
 
-    calculateChunkSize() {
-        // Динамический размер чанка на основе длины ключа
-        return this.keyLength <= 5 ? 1e5 : 
-               this.keyLength <= 7 ? 1e4 : 
-               this.keyLength <= 9 ? 1e3 : 100;
-    }
-
-    reportProgress() {
-        self.postMessage({
-            type: 'progress',
-            keysTested: this.keysTested,
-            kps: Math.round(this.keysTested / ((performance.now() - this.startTime) / 1000))
-        });
+    reportProgressIfNeeded() {
+        const now = performance.now();
+        if (now - this.lastReportTime > 1000) {
+            this.lastReportTime = now;
+            const kps = (this.keysTested / ((now - this.startTime) / 1000)) | 0;
+            self.postMessage({
+                type: 'progress',
+                keysTested: this.keysTested,
+                kps
+            });
+        }
     }
 }
 
-new K4TurboWorker();
+new K4Worker();
