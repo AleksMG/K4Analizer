49d04b82dcaae66963fb5775be5d18170a1b4516
AleksMG <5978497@gmail.com>
2025-05-20 02:20:58 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -166,7 +166,7 @@ class K4Worker {
         const keysPerWorker = Math.ceil(this.totalKeysToTest / this.totalWorkers);
         const startKey = this.workerId * keysPerWorker;
         const endKey = Math.min(startKey + keysPerWorker, this.totalKeysToTest);
-        const BLOCK_SIZE = 1000;
+        const BLOCK_SIZE = 10000;
 
         while (this.running && !this.completed) {
             switch (this.mode) {
